c124357d2499b7ea84967d0791acd648adf1028e
KhalidAmrTammam <khalidtammam062@gmail.com>
2025-07-22 01:15:52 +0300

    Created the project and its main folders successfully

---
+++ MoviesApp/PresentationLayer/MovieListingScreen/MovieListingScreenViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MovieListingScreenViewController.swift
+//  MoviesApp
+//
+//  Created by Khalid Amr on 22/07/2025.
+//
+
+import UIKit
+
+class MovieListingScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
